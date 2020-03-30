bc6b60e46201b1a6b602d2663459c6f9b1f09b79
Daniel Marques <danielmarquesvg@gmail.com>
2020-03-30 20:48:01 -0300

    Atualização de dados - 30/03/2020 - 17h
    Novo boletim da secretaria de saúde do estado da Paraíba, referente ao dia 30/03/2020, publicado as 17h

--- assets/js/visualizacao_facet_paciente_idade.js
+++ assets/js/visualizacao_facet_paciente_idade.js
@@ -1,7 +1,7 @@
 var viewFacetPacienteIdade = {
     "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
     "title": "Classificação por idade divididos pelo sexo",
-    "width": 300,
+    "width": 250,
     "height": 200,
     "data": {
         "url": "https://raw.githubusercontent.com/covid19pb/covid19pb.github.io/master/data/dados_pb_covid19_casosPorPessoa.csv",
