ccfe96fd7f8951ef9c684459b02c3a3cee683a31
Daniel Marques <danielmarquesvg@gmail.com>
2020-04-08 19:17:52 -0300

    Correção da escala de cores do mapa das cidades
    Anteriormente, foi colocado mais parâmetros no "domain", mas não foi adicionado a mesma quantidade destes parâmetros no "range"

--- assets/js/visualizacao_cidades.js
+++ assets/js/visualizacao_cidades.js
@@ -32,7 +32,7 @@ var viewCidades = {
 			"type": "quantitative", 
 			"title": "Casos confirmados",
 			"scale": {
-				"domain" : [0, 1, 2, 3, 6, 40],
+				"domain" : [0, 2, 6, 40],
 				"range": ["#2EC4B6", "#FF9F1C", "#E71D36", "#011627"]
 			},
 			"legend": {
