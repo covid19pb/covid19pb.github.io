1f402a3a984621cc600d06aad40da8eb4381ba54
francinaldocn <38075198+francinaldocn@users.noreply.github.com>
2020-04-12 16:02:35 -0300

    Atualização das visualizações

--- assets/js/visualizacao_diaria.js
+++ assets/js/visualizacao_diaria.js
@@ -1,61 +1,75 @@
 var viewDiaria = {
-	"$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-	"width": 300,
-	"height": 200,
-	"data": {
-		"url": "https://raw.githubusercontent.com/covid19pb/covid19pb.github.io/master/data/dados_pb_covid19_casosPorData.csv",
-		"format": {"type": "csv"}
-	},
-	"transform": [{"fold": ["novosCasos", "totalDeCasos","mortes"]}],
-	"mark": {
-		"type": "line",
-		"point": {
-			"filled": false,
-			"fill": "white"
-		}
-	},
-	"encoding": {
-		"x": {
-			"field": "data",
-			"type": "temporal",
-			"timeUnit": {
-			    "unit": "monthdate"
-			},
-			"axis": {
-				"title": "Data (em dias)",
-				"grid": true
-			}
-		},
-		"y": {
-			"field": "value",
-			"type": "quantitative",
-			"axis": {
-				"title": "Quantidade de casos",
-				"grid": true
-			}
-		},
-		"color": {
-			"field": "key",
-			"type": "nominal",
-			"title":"Tipos de casos",
-			"legend": {
-				"direction" : "horizontal",
-				"orient" : "bottom"
-			}
-		},
-		"tooltip": [
-		{
-			"field": "value",
-			"type" : "quantitative",
-			"title" : "Quantidade de casos"
-		},
-		{
-			"field": "data",
-			"type" : "temporal",
-			"title" : "Data"
-		}
-		]
-	}
-				  
+  "$schema": "https://vega.github.io/schema/vega-lite/v4.0.2.json",
+  "width": 300,
+    "height": 200,
+  "config": {
+    "background": "transparent",
+    "view": {"continuousWidth": 400, "continuousHeight": 300}
+  },
+  "data": {
+    "url": "https://raw.githubusercontent.com/covid19pb/covid19pb.github.io/master/data/dados_pb_covid19_casosPorData.csv"
+  },
+  "mark": {
+    "type": "line",
+    "opacity": 0.8,
+    "point": {"filled": false, "fill": "white"},
+    "strokeWidth": 2.5
+  },
+  "encoding": {
+    "color": {
+      "type": "nominal",
+      "field": "key",
+      "legend": {
+        "columns": 3,
+        "labelFontSize": 12,
+        "orient": "bottom",
+        "title": "Tipos de Casos",
+        "titleFontSize": 14
+      },
+      "scale": {
+        "domain": ["Total de Casos", "Novos Casos", "Mortes"],
+        "range": ["#ff8533", "#1f77b4", "#e60000"]
+      }
+    },
+    "tooltip": [
+      {"type": "quantitative", "field": "value", "title": "Número de Casos"},
+      {"type": "temporal", "field": "data", "title": "Data"}
+    ],
+    "x": {
+      "type": "temporal",
+      "axis": {
+        "labelAngle": 315,
+        "labelFontSize": 12,
+        "title": "Data",
+        "titleFontSize": 14
+      },
+      "field": "data",
+      "timeUnit": "monthdate"
+    },
+    "y": {
+      "type": "quantitative",
+      "axis": {
+        "labelFontSize": 12,
+        "title": "Número de Casos",
+        "titleFontSize": 14
+      },
+      "field": "value"
+    }
+  },
+  "transform": [
+    {"fold": ["novosCasos", "totalDeCasos", "mortes"]},
+    {
+      "calculate": "if((datum.key === 'novosCasos'),'Novos Casos',datum.key)",
+      "as": "key"
+    },
+    {
+      "calculate": "if((datum.key === 'totalDeCasos'),'Total de Casos',datum.key)",
+      "as": "key"
+    },
+    {
+      "calculate": "if((datum.key === 'mortes'),'Mortes',datum.key)",
+      "as": "key"
+    }
+  ]
 };
 vegaEmbed('#visualizacao_diaria', viewDiaria);
--- assets/js/visualizacao_diaria_descartados_suspeitos.js
+++ assets/js/visualizacao_diaria_descartados_suspeitos.js
@@ -1,61 +1,79 @@
 var viewDiariaDescartadosSuspeitos = {
-	"$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-	"width": 300,
-	"height": 200,
-	"data": {
-		"url": "https://raw.githubusercontent.com/covid19pb/covid19pb.github.io/master/data/dados_pb_covid19_casosPorData.csv",
-		"format": {"type": "csv"}
-	},
-	"transform": [{"fold": ["descartados", "suspeitosInternados","notificacoesSuspeitos"]}],
-	"mark": {
-		"type": "line",
-		"point": {
-			"filled": false,
-			"fill": "white"
-		}
-	},
-	"encoding": {
-		"x": {
-			"field": "data",
-			"type": "temporal",
-			"timeUnit": {
-			    "unit": "monthdate"
-			},
-			"axis": {
-				"title": "Data (em dias)",
-				"grid": true
-			}
-		},
-		"y": {
-			"field": "value",
-			"type": "quantitative",
-			"axis": {
-				"title": "Quantidade de casos",
-				"grid": true
-			}
-		},
-		"color": {
-			"field": "key",
-			"type": "nominal",
-			"title":"Tipos de casos",
-			"legend": {
-				"direction" : "horizontal",
-				"orient" : "bottom"
-			}
-		},
-		"tooltip": [
-		{
-			"field": "value",
-			"type" : "quantitative",
-			"title" : "Quantidade de casos"
-		},
-		{
-			"field": "data",
-			"type" : "temporal",
-			"title" : "Data"
-		}
-		]
-	}
-				  
+  "$schema": "https://vega.github.io/schema/vega-lite/v4.0.2.json",
+  "width": 300,
+  "height": 200,
+  "config": {
+    "background": "transparent",
+    "view": {"continuousWidth": 400, "continuousHeight": 300}
+  },
+  "data": {
+    "url": "https://raw.githubusercontent.com/covid19pb/covid19pb.github.io/master/data/dados_pb_covid19_casosPorData.csv"
+  },
+  "mark": {
+    "type": "line",
+    "opacity": 0.8,
+    "point": {"filled": false, "fill": "white"},
+    "strokeWidth": 2.5
+  },
+  "encoding": {
+    "color": {
+      "type": "nominal",
+      "field": "key",
+      "legend": {
+        "columns": 1,
+        "labelFontSize": 12,
+        "orient": "bottom",
+        "title": "Tipos de Casos",
+        "titleFontSize": 14
+      },
+      "scale": {
+        "domain": [
+          "Descartados",
+          "Suspeitos Notificados",
+          "Suspeitos Internados"
+        ],
+        "range": ["#1f77b4", "#ff8533", "#e60000"]
+      }
+    },
+    "tooltip": [
+      {"type": "quantitative", "field": "value", "title": "Número de Casos"},
+      {"type": "temporal", "field": "data", "title": "Data"}
+    ],
+    "x": {
+      "type": "temporal",
+      "axis": {
+        "labelAngle": 315,
+        "labelFontSize": 12,
+        "title": "Data",
+        "titleFontSize": 14
+      },
+      "field": "data",
+      "timeUnit": "monthdate"
+    },
+    "y": {
+      "type": "quantitative",
+      "axis": {
+        "labelFontSize": 12,
+        "title": "Número de Casos",
+        "titleFontSize": 14
+      },
+      "field": "value"
+    }
+  },
+  "transform": [
+    {"fold": ["descartados", "suspeitosInternados", "notificacoesSuspeitos"]},
+    {
+      "calculate": "if((datum.key === 'descartados'),'Descartados',datum.key)",
+      "as": "key"
+    },
+    {
+      "calculate": "if((datum.key === 'notificacoesSuspeitos'),'Suspeitos Notificados',datum.key)",
+      "as": "key"
+    },
+    {
+      "calculate": "if((datum.key === 'suspeitosInternados'),'Suspeitos Internados',datum.key)",
+      "as": "key"
+    }
+  ]
 };
 vegaEmbed('#visualizacao_diaria_descartados_suspeitos', viewDiariaDescartadosSuspeitos);
