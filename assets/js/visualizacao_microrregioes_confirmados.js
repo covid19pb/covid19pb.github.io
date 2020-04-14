85fd4fdb8bdcbcd31039131eeb7ed06f9a0da76b
Daniel Marques <danmarquesvg@gmail.com>
2020-04-14 18:51:28 -0300

    Atualização dos dados - 14/04/2020
    Boletim da Secretaria de Saúde da PB
    Data: 14/04/2020
    Horário: 17h

--- assets/js/visualizacao_microrregioes_confirmados.js
+++ assets/js/visualizacao_microrregioes_confirmados.js
@@ -32,7 +32,7 @@ var viewMicrorregioesConfirmados = {
 			"type": "quantitative", 
 			"title": "Casos confirmados",
 			"scale": {
-				"domain" : [0, 1, 3, 5, 50, 125],
+				"domain" : [0, 1, 3, 5, 50, 140],
 				"range": ["#F0F0F0", "#FFB000", "#FE6100", "#DC267F", "#785EF0","#648FFF"]
 			},
 			"legend": {
