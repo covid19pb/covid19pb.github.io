4670864f915a1c034f8a52a9c83638fa15336f96
Daniel Marques <danmarquesvg@gmail.com>
2020-05-03 19:54:53 -0300

    Atualização dos dados - 03/05/2020
    Boletim da secretaria de saúde da PB
    Data: 03/05/2020
    Horário: 18h
    
    Outras observações
    - Adicionado a visualização de mortes acumuladas por faixa etária por dia

---
+++ assets/js/visualizacao_mortes_por_dia_por_faixa_etaria.js
@@ -0,0 +1,50 @@
+var viewMortesPorDiaPorFaixaEtaria = {
+	"$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+	  "transform": [{"filter": "datum.classificacao === 'mortos'"}],
+	  "width": "container",
+	  "data": {
+	    "url": "https://raw.githubusercontent.com/covid19pb/covid19pb.github.io/master/data/dados_pb_covid19_boletim_FaixaEtaria.csv"
+	  },
+	  "mark": "rect",
+	  "encoding": {
+	    "x": {
+	      "timeUnit": "monthdate",
+	      "field": "data",
+	      "type": "ordinal",
+	      "axis": {
+	        "labelAngle": 315,
+	          "labelFontSize": 12,
+	          "title": "Data",
+	          "titleFontSize": 14
+	      }
+	    },
+	    "y": {
+	      "field": "faixa_etaria",
+	      "type": "nominal",
+	      "title": "faixa etária",
+	      "sort" : "descending",
+	      "axis": {
+	          "labelFontSize": 12,
+	          "title": "Faixa Etária",
+	          "titleFontSize": 14
+	        }
+	    },
+	    "color": {
+	      "field": "total",
+	      "type": "quantitative",
+	      "legend": {
+	          "columns": 1,
+	          "labelFontSize": 12,
+	          "orient": "bottom",
+	          "title": "Óbitos",
+	          "titleFontSize": 14
+	        },
+	      "scale": {
+					"type":"threshold",
+					"domain" : [1, 5, 10, 15, 20, 25],
+					"range": ["#F0F0F0", "#FFBD00", "#FF5400", "#FF0054", "#9E0059", "#390099","#000000"]
+				}
+	    }
+	  }
+};
+vegaEmbed('#visualizacao_mortes_por_dia_por_faixa_etaria', viewMortesPorDiaPorFaixaEtaria);
