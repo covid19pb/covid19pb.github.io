e3f41698104b34c48ff4d7de17f04af4dbb18c32
Daniel Marques <danmarquesvg@gmail.com>
2020-06-06 21:03:16 -0300

    Atualização dos dados - 06/06/2020
    Boletim da secretaria de saúde da PB
    Data: 06/06/2020
    Horário: 18h
    
    Outras observações
    - Inserção da visualização dos locais dos óbitos

---
+++ assets/js/visualizacao_local_obito.js
@@ -0,0 +1,103 @@
+var viewLocalObito = {
+	"$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+	"data": {
+		"url": "https://raw.githubusercontent.com/covid19pb/covid19pb.github.io/master/data/dados_pb_covid19_pacientes_mortes.csv"
+  	},
+  	"width": "container",
+  	"height": "300",
+    "config": {
+    	"background": "transparent",
+    	"view": {"continuousWidth": 400, "continuousHeight": 300}
+    },
+  	"transform": [
+        {
+          "calculate": "if((datum.localDoObito === ''),'Não Identificado',datum.key)",
+          "as": "key"
+        },
+        {
+          "calculate": "if((datum.localDoObito === 'SAMU'),'SAMU',datum.key)",
+          "as": "key"
+        },
+		{
+          "calculate": "if((datum.localDoObito === 'UPA'),'UPA',datum.key)",
+          "as": "key"
+        },
+		{
+          "calculate": "if((datum.localDoObito === 'hospital privado'),'Hospital Privado',datum.key)",
+          "as": "key"
+        },
+		{
+          "calculate": "if((datum.localDoObito === 'hospital público'),'Hospital Público',datum.key)",
+          "as": "key"
+        },
+		{
+          "calculate": "if((datum.localDoObito === 'maternidade pública'),'Maternidade Pública',datum.key)",
+          "as": "key"
+        },
+		{
+          "calculate": "if((datum.localDoObito === 'residência'),'Residência',datum.key)",
+          "as": "key"
+        }
+	],
+	"encoding": {
+		"x": {
+			"axis": {
+				"title": "Quantidade de óbitos",
+				"labelFontSize": 10,
+				"titleFontSize": 15
+				},
+				"field": "key",
+			"type": "quantitative",
+			"aggregate": "count"
+		},
+		"y": {
+			"axis": {
+				"labelFontSize": 14,
+				"titleFontSize": 14,
+				"title": null
+			},
+			"field": "key",
+			"type": "nominal",
+			"sort": "x"
+		}
+	},
+	"layer": [
+		{
+			"mark": {
+				"type": "bar",
+				"cornerRadiusEnd": 4,
+				"size": 30
+			},
+			"encoding": {
+				"color": {
+					"legend": null,
+					"field": "key",
+					"type": "nominal",
+					"scale": {
+						"domain": ["SAMU","Não Identificado","UPA", "Hospital Privado", "Hospital Público", "Maternidade Pública", "Residência"],
+	  					"range": ["#FE0312","#FFBD00", "#FF5400", "#FF0054", "#9E0059", "#390099","#000000"]
+					}
+				},
+				"opacity": {"value": 0.7}
+			}
+		},
+		{
+	  		"mark": {
+	    		"type": "text",
+	    		"align": "left",
+	    		"baseline": "middle",
+	    		"stroke": "black",
+	    		"dx": 2,
+	    		"fontSize": 20
+	  		},
+  			"encoding": {
+		    	"text": {
+		    		"field": "key",
+		    		"type": "quantitative",
+		    		"aggregate": "count"
+		    	}
+  			}
+		}
+	]
+};
+vegaEmbed('#visualizacao_local_obito', viewLocalObito);
